166454761d461de8f769c39c5630f0fc595f5de1
Smx27 <64015856+Smx27@users.noreply.github.com>
2026-03-14 23:22:15 +0530

    Add Terms of Service page and footer legal links

---
+++ apps/report-web/app/terms/page.tsx
@@ -0,0 +1,9 @@
+import { TermsOfService } from '../../components/terms/TermsOfService';
+
+export default function TermsPage() {
+  return (
+    <div className="px-4 sm:px-6 lg:px-8">
+      <TermsOfService />
+    </div>
+  );
+}
